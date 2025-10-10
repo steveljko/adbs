d97a468f1296eff590b9b5dc9010610288931809
steveljko <steveljko@proton.me>
2025-10-10 20:41:45 +0200

    add 'cheveron right' icon and change card style

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,19 +1,19 @@
 @props(['header' => null, 'footer' => null])
 
-<div {{ $attributes->merge(['class' => 'bg-white rounded-lg shadow-md']) }}>
-    @if($header || isset($header))
-    <div class="flex items-center justify-between p-4 bg-gray-100 border-b border-gray-200">
-        {{ $header }}
-    </div>
+<div {{ $attributes->merge(['class' => 'bg-white border rounded-lg shadow-sm']) }}>
+    @if ($header || isset($header))
+        <div class="flex items-center justify-between border-gray-200 px-4 pt-3">
+            {{ $header }}
+        </div>
     @endif
 
     <div class="p-4">
         {{ $slot }}
     </div>
 
-    @if($footer || isset($footer))
-    <div class="px-4 py-3 bg-gray-50 border-t border-gray-200">
-        {{ $footer }}
-    </div>
+    @if ($footer || isset($footer))
+        <div class="border-t border-gray-200 px-4 py-2">
+            {{ $footer }}
+        </div>
     @endif
 </div>
---
+++ resources/views/components/icons/chevron-right.blade.php
@@ -0,0 +1,6 @@
+<path
+    d="M9 5l7 7-7 7"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+/>
